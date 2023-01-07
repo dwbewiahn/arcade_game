eeb8db1a81e1f39166f9b1236000e1a682d48511
Douglas <dwbewiahn@gmail.com>
2023-01-07 18:08:59 +0000

    initial configurations

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("I hate Hello World")
+#Python arcade game style 
+import pygame
+import math
+
+pygame.init()
+fps = 60 
+timer = pygame.time.clock()
+font = pygame.font.Font('assets/font/myFont.ttf',32)
+WIDTH = 900
+HEIGHT = 800
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+bgs = []
+banners = []
+guns = []
+level = 0
+
+
+
+
+
