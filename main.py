c669c40fa5178c4b7ea7f4aadb7ba1ecd4e952e5
Douglas <dwbewiahn@gmail.com>
2023-01-07 18:13:32 +0000

    load assets for the game

--- main.py
+++ main.py
@@ -13,6 +13,12 @@ bgs = []
 banners = []
 guns = []
 level = 0
+for i in range (1,4):
+    bgs.append(pygame.image.load(f'assets/bgs/{i}.png'))
+    banners.append(pygame.image.load(f'assets/banners/{i}.png'))
+    guns.append(pygame.image.load(f'assets/guns/{i}.png'))
+
+    
 
 
 
