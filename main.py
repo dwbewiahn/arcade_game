135f52558c07de673d168c06048251c53aa848fa
Douglas <dwbewiahn@gmail.com>
2023-01-08 08:07:43 +0000

    relative path fix

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import math
 pygame.init()
 fps = 60 
 timer = pygame.time.Clock()
-font = pygame.font.Font('assets\font\myFont.ttf',32)
+font = pygame.font.Font('assets/font/myFont.ttf',32)
 WIDTH = 900
 HEIGHT = 800
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
@@ -14,9 +14,9 @@ banners = []
 guns = []
 level = 0
 for i in range (1,4):
-    bgs.append(pygame.image.load(f'assets\bgs\{i}.png'))
-    banners.append(pygame.image.load(f'assets\banners\{i}.png'))
-    guns.append(pygame.image.load(f'assets\guns\{i}.png'))
+    bgs.append(pygame.image.load(f'assets/bgs/{i}.png'))
+    banners.append(pygame.image.load(f'assets/banners/{i}.png'))
+    guns.append(pygame.image.load(f'assets/guns/{i}.png'))
 
 run = True
 while run:
